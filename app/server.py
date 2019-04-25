8dd183a1f62d66ccaf2c441d9cf5b3dd5548d867
giglesiasy <49966205+giglesiasy@users.noreply.github.com>
2019-04-24 22:16:29 -0400

    Update server.py

--- app/server.py
+++ app/server.py
@@ -9,7 +9,7 @@ from fastai import *
 from fastai.vision import *
 
 # export_file_url = 'https://www.dropbox.com/s/v6cuuvddq73d1e0/export.pkl?raw=1'
-export_file_url = 'https://drive.google.com/file/d/1Nvfmf_-hfsPNSMlhxqFk2o_TW9g3C4K_'
+export_file_url = 'https://drive.google.com/uc?export=download&id=1Nvfmf_-hfsPNSMlhxqFk2o_TW9g3C4K_'
 export_file_name = 'export.pkl'
 
 classes = ['zorzal', 'chercan', 'chincol', 'cachudito']
